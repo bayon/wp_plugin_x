93e969d2e044c141e3d97675e72e30c3021c0806
bayonAtpark <bayon.forte@parkseed.com>
2017-12-16 11:12:00 -0500

    initial commit

---
+++ wp_plugin_x.php
@@ -0,0 +1,23 @@
+
+/*
+Plugin Name:  wp plugin x
+Plugin URI:    https://github.com/bayon/wp_plugin_x
+Description:   Absolute most basic plugin.
+Version:      1.0.0
+Author:       sprite-pilot.com
+Author URI:   https://www.sprite-pilot.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:   wp_plugin_x
+Domain Path:  /languages
+
+GitHub Plugin URI: https://github.com/bayon/wp_plugin_x
+*/
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+ ?>
+
+ <?php
+echo("<br>".__FILE__);
+echo("<br>WP_PLUGIN_X");
+  ?>
